be15a25d9ae5ca87763e96c8faaa34c571601538
wantop <wantop@jbnu.ac.kr>
2023-02-09 19:25:48 +0900

    feat : use-http

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,40 +1,8 @@
-import { useState, useEffect } from "react";
 import TodoItem from "../Todos/TodoItem";
 import Card from "../UI/Card/Card";
 import LoadingSpinner from "../UI/Progress/LodingSpinner";
 
-const REQUEST_URL = "https://pre-onboarding-selection-task.shop";
-
-const TodoList = () => {
-  const token = localStorage.getItem("token");
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    const fetchTodos = async () => {
-      setLoading(true);
-      const response = await fetch(`${REQUEST_URL}/todos`, {
-        method: "GET",
-        headers: { Authorization: "Bearer " + token },
-      });
-
-      if (!response.ok) {
-        throw new Error("Can not load todo list!");
-      }
-      const data = await response.json();
-      setTodos(data);
-      setLoading(false);
-    };
-    fetchTodos().catch((error) => {
-      setError(error.message);
-      setLoading(false);
-    });
-  }, [token]);
-
-  useEffect(() => {
-  }, [todos]);
-
+const TodoList = ({todos, error, isLoading}) => {
   const todoList = todos.map((item) => (
     <TodoItem
       key={item.id}
@@ -47,9 +15,9 @@ const TodoList = () => {
   return (
     <section>
       <Card>
-        {loading && <LoadingSpinner />}
-        {!loading && error && <p>{error}</p>}
-        {!loading && !error && <ul>{todoList}</ul>}
+        {isLoading && <LoadingSpinner />}
+        {!isLoading && error && <p>{error}</p>}
+        {!isLoading && !error && <ul>{todoList}</ul>}
       </Card>
     </section>
   );
--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
 import CreateTodo from "../components/Todos/CreateTodo";
 import TodoList from "../components/Todos/TodoList";
+import useHttp from "../hooks/use-http";
+
+const REQUEST_URL = "https://pre-onboarding-selection-task.shop";
 
 const Todo = () => {
+  const token = localStorage.getItem("token");
+  const [todos, setTodos] = useState([]);
+  const { isLoading, error, sendRequest: fetchTodos } = useHttp();
+
+  useEffect(() => {
+    fetchTodos(
+      {
+        url: `${REQUEST_URL}/todos`,
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      },
+      setTodos
+    );
+  }, [fetchTodos, token]);
+
   return (
     <>
       <CreateTodo />
-      <TodoList />
+      <TodoList todos={todos} error={error} isLoading={isLoading} />
     </>
   );
 };
